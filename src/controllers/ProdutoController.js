7ad3087aaaa7e3c2a42547d2620e2e1ead427fab
Davi Trigueiro Sampaio <davi.sampaio@ivia.corp>
2019-06-13 16:56:39 -0300

    Novas dependencias e criação da entidade Produto e seu Controller

---
+++ src/controllers/ProdutoController.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Produto = mongoose.model('Produto');
+
+module.exports = {
+    async index(req,res) {
+        const produtos = await Produto.find();
+
+        return res.json(produtos);
+    }
+};
---
+++ src/models/Produto.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const ProdutoSchema = new mongoose.Schema({
+    titulo: {
+        type: String,
+        required: true,
+    },
+    descricao: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+mongoose.model("Produto", ProdutoSchema);
